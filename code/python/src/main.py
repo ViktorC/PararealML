53e13bb328fd837006918950d1abf70e09ce9d9e
Viktor Csomor <viktor.csomor@gmail.com>
2020-03-23 02:29:24 +0000

    Add a main script for running the Parareal solver

---
+++ code/python/src/main.py
@@ -0,0 +1,17 @@
+from diff_eq import RabbitPopulationDiffEq
+from runge_kutta import ForwardEulerMethod, RK4
+from parareal import Parareal
+
+
+diff_eq = RabbitPopulationDiffEq(10000., .01)
+
+fine_integrator = RK4()
+coarse_integrator = ForwardEulerMethod()
+
+fine_d_t = .25
+coarse_d_t = 2 * fine_d_t
+t_max = 8.
+k = 2
+
+parareal = Parareal(diff_eq, fine_integrator, coarse_integrator, fine_d_t, coarse_d_t, t_max, k)
+parareal.run()
