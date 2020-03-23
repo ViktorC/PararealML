fbdfe2d8e39ac68bdcd7278ddb2f9e2a2efa9328
Viktor Csomor <viktor.csomor@gmail.com>
2020-03-23 01:51:29 +0000

    Add a base class for Runge-Kutta methods

--- code/python/src/runge_kutta.py
+++ code/python/src/runge_kutta.py
@@ -1,30 +1,42 @@
 from typing import Callable
 
 
-class ForwardEulerMethod:
+class RungeKuttaMethod:
+    """
+    A base class for Runge-Kutta methods
+    """
+
+    """
+    Estimates the value of y at x^ = x + d_x.
+    """
+    def integrate(self, y: float, x: float, d_x: float, d_y_wrt_x: Callable[[float, float], float]) -> float:
+        pass
+
+
+class ForwardEulerMethod(RungeKuttaMethod):
     """
     The forward Euler method, an explicit first order Runge-Kutta method.
     """
 
-    def __call__(self, y: float, x: float, d_x: float, d_y_wrt_x: Callable[[float, float], float]) -> float:
+    def integrate(self, y: float, x: float, d_x: float, d_y_wrt_x: Callable[[float, float], float]) -> float:
         return y + d_x * d_y_wrt_x(x, y)
 
 
-class ExplicitMidpointMethod:
+class ExplicitMidpointMethod(RungeKuttaMethod):
     """
     The explicit midpoint method, a second order Runge-Kutta method.
     """
 
-    def __call__(self, y: float, x: float, d_x: float, d_y_wrt_x: Callable[[float, float], float]) -> float:
+    def integrate(self, y: float, x: float, d_x: float, d_y_wrt_x: Callable[[float, float], float]) -> float:
         return y + d_x * d_y_wrt_x(x + d_x / 2., y + d_y_wrt_x(x, y) * d_x / 2.)
 
 
-class RK4:
+class RK4(RungeKuttaMethod):
     """
     The RK4 method, an explicit fourth order Runge-Kutta method.
     """
 
-    def __call__(self, y: float, x: float, d_x: float, d_y_wrt_x: Callable[[float, float], float]) -> float:
+    def integrate(self, y: float, x: float, d_x: float, d_y_wrt_x: Callable[[float, float], float]) -> float:
         k1 = d_x * d_y_wrt_x(x, y)
         k2 = d_x * d_y_wrt_x(x + d_x / 2., y + k1 / 2.)
         k3 = d_x * d_y_wrt_x(x + d_x / 2., y + k2 / 2.)
--- code/python/tests/test_runge_kutta.py
+++ code/python/tests/test_runge_kutta.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.runge_kutta import ForwardEulerMethod, ExplicitMidpointMethod, RK4
+from runge_kutta import ForwardEulerMethod, ExplicitMidpointMethod, RK4
 
 
 def test_forward_euler_method():
@@ -13,7 +13,7 @@ def test_forward_euler_method():
     def d_y_wrt_x(x, y): return 5 * y + x ** 2
 
     expected_y2 = 4.
-    actual_y2 = euler(y1, x1, d_x, d_y_wrt_x)
+    actual_y2 = euler.integrate(y1, x1, d_x, d_y_wrt_x)
 
     assert actual_y2 == pytest.approx(expected_y2)
 
@@ -28,7 +28,7 @@ def test_explicit_midpoint_method():
     def d_y_wrt_x(x, y): return 2 * y - 4 * x
 
     expected_y2 = 4.5
-    actual_y2 = midpoint(y1, x1, d_x, d_y_wrt_x)
+    actual_y2 = midpoint.integrate(y1, x1, d_x, d_y_wrt_x)
 
     assert actual_y2 == pytest.approx(expected_y2)
 
@@ -43,6 +43,6 @@ def test_rk4():
     def d_y_wrt_x(x, y): return 2 * y + 1
 
     expected_y2 = 3.
-    actual_y2 = rk4(y1, x1, d_x, d_y_wrt_x)
+    actual_y2 = rk4.integrate(y1, x1, d_x, d_y_wrt_x)
 
     assert actual_y2 == pytest.approx(expected_y2)
