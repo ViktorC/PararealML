147ab4c84f535a39b2533ad1b5993cd974e098c9
Viktor Csomor <viktor.csomor@gmail.com>
2020-03-24 02:27:33 +0000

    Use a linear regression based ML operator as G

--- code/python/src/main.py
+++ code/python/src/main.py
@@ -1,11 +1,14 @@
+from sklearn.linear_model import LinearRegression
+
 from src.diff_eq import RabbitPopulationDiffEq
-from src.operator import ConventionalOperator
-from src.integrator import ForwardEulerMethod, RK4
+from src.operator import ConventionalOperator, MLOperator
+from src.integrator import ExplicitMidpointMethod, RK4
 from src.parareal import Parareal
 
 
-diff_eq = RabbitPopulationDiffEq(10000., 5e-2, 0., 1.)
+diff_eq = RabbitPopulationDiffEq(10000., 5e-2, 0., 100.)
 f = ConventionalOperator(RK4(), .025)
-g = ConventionalOperator(ForwardEulerMethod(), .1)
+g_trainer = ConventionalOperator(ExplicitMidpointMethod(), .1)
+g = MLOperator(LinearRegression(), g_trainer, .5)
 solver = Parareal(f, g, 2)
 solver.solve(diff_eq)
