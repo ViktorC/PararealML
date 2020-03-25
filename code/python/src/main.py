a6ff6cf1a37143af89a62e0dab45d69b8bb7bff5
Viktor Csomor <viktor.csomor@gmail.com>
2020-03-25 04:04:05 +0000

    Move timing and result analysis out of the parareal class

--- code/python/src/main.py
+++ code/python/src/main.py
@@ -6,25 +6,46 @@ from src.integrator import ForwardEulerMethod, RK4
 from src.operator import ConventionalOperator, MLOperator
 from src.parareal import Parareal
 
-diff_eq = RabbitPopulationDiffEq(1000., 1e-4, 0., 50000.)
-f = ConventionalOperator(RK4(), .01)
-g = ConventionalOperator(ForwardEulerMethod(), .1)
-k = 1
 
-solver = Parareal(f, g, k)
-solver.solve(diff_eq)
-
-ml_g = MLOperator(LinearRegression(), g, 100., 10)
-ml_g.train_model(diff_eq)
-
-ml_solver = Parareal(f, ml_g, k)
-ml_solver.solve(diff_eq)
-
-if MPI.COMM_WORLD.rank == 0:
+def time_parallel_solver_and_print_result(parallel_solver, solver_name):
+    comm.barrier()
     start_time = MPI.Wtime()
-    y_x_max = f.trace(
+    y_max = parallel_solver.solve(diff_eq)[-1]
+    comm.barrier()
+    end_time = MPI.Wtime()
+    if comm.rank == 0:
+        print(f'{solver_name} solution: {y_max}; '
+              f'execution took {end_time - start_time}s')
+
+
+def time_operator_and_print_result(operator, operator_name):
+    start_time = MPI.Wtime()
+    y_max = operator.trace(
         diff_eq, diff_eq.y_0(), diff_eq.x_0(), diff_eq.x_max())[-1]
     end_time = MPI.Wtime()
+    print(f'{operator_name} solution: {y_max}; '
+          f'execution took {end_time - start_time}s')
 
-    print(f'Serial fine solution: {y_x_max}')
-    print(f'Serial execution took {end_time - start_time}s')
+
+comm = MPI.COMM_WORLD
+
+diff_eq = RabbitPopulationDiffEq(1000., 1e-4, 0., 50000.)
+
+f = ConventionalOperator(RK4(), .01)
+g = ConventionalOperator(ForwardEulerMethod(), .1)
+g_ml = MLOperator(LinearRegression(), g, 100., 10)
+
+k = 1
+
+parareal_ml = Parareal(f, g_ml, k)
+parareal = Parareal(f, g, k)
+
+time_parallel_solver_and_print_result(parareal_ml, 'Parareal ML w/ training')
+time_parallel_solver_and_print_result(parareal_ml, 'Parareal ML w/o training')
+time_parallel_solver_and_print_result(parareal, 'Parareal')
+
+if comm.rank == 0:
+    print(f'Analytic solution: {diff_eq.exact_y(diff_eq.x_max())}')
+    time_operator_and_print_result(f, 'Fine')
+    time_operator_and_print_result(g, 'Coarse')
+    time_operator_and_print_result(g_ml, 'Coarse ML')
--- code/python/src/parareal.py
+++ code/python/src/parareal.py
@@ -29,79 +29,43 @@ class Parareal:
         self.g = g
         self.k = k
 
-    def _print_results(
-            self,
-            diff_eq: OrdinaryDiffEq,
-            time_slices: Sequence[float],
-            y_coarse: Sequence[float],
-            y: Sequence[float],
-            y_trajectory: Sequence[float]):
-        print('Coarse solution\n', y_coarse)
-        print('Fine solution\n', y)
-
-        print_trajectory = len(y_trajectory) <= 50
-
-        if diff_eq.has_exact_solution():
-            y_exact = np.empty(len(time_slices))
-            y_exact[0] = diff_eq.y_0()
-
-            for i, t in enumerate(time_slices[1:]):
-                y_exact[i + 1] = diff_eq.exact_y(t)
-            print('Analytic solution\n', y_exact)
-
-            if print_trajectory:
-                y_exact_trajectory = np.empty(len(y_trajectory))
-
-                for i, t in enumerate(
-                        np.linspace(
-                            diff_eq.x_0() + self.f.d_x(),
-                            diff_eq.x_max(),
-                            len(y_exact_trajectory))):
-                    y_exact_trajectory[i] = diff_eq.exact_y(t)
-                print('Analytic trajectory:\n', y_exact_trajectory)
-
-        if print_trajectory:
-            print('Fine trajectory:\n', y_trajectory)
-
-    def solve(
-            self,
-            diff_eq: OrdinaryDiffEq,
-            verbose: bool = True) -> Sequence[float]:
+    def solve(self, diff_eq: OrdinaryDiffEq) -> Sequence[float]:
         """
         Runs the Parareal solver and returns the discretised solution of the
         differential equation.
 
         :param diff_eq: the differential equation to solve
-        :param verbose: whether information about the solution and the
-        execution time should be printed
         :return: the discretised trajectory of the differential equation's
         solution
         """
         comm = MPI.COMM_WORLD
-        comm.barrier()
-        start_time = MPI.Wtime()
 
-        rank = comm.Get_rank()
-        size = comm.Get_size()
-        time_slices = np.linspace(diff_eq.x_0(), diff_eq.x_max(), size + 1)
-
+        time_slices = np.linspace(
+            diff_eq.x_0(), diff_eq.x_max(), comm.size + 1)
         y = np.empty(len(time_slices))
         y_trajectory = np.empty(
-            (size, int(time_slices[-1] / (size * self.f.d_x()))))
+            (comm.size, int(time_slices[-1] / (comm.size * self.f.d_x()))))
         y[0] = diff_eq.y_0()
 
         for i, t in enumerate(time_slices[:-1]):
             y[i + 1] = self.g.trace(diff_eq, y[i], t, time_slices[i + 1])[-1]
-        y_coarse = np.copy(y) if rank == 0 else None
 
-        for i in range(min(size, self.k)):
+        for i in range(min(comm.size, self.k)):
             my_f_trajectory = self.f.trace(
-                diff_eq, y[rank], time_slices[rank], time_slices[rank + 1])
-            for j in range(size):
+                diff_eq,
+                y[comm.rank],
+                time_slices[comm.rank],
+                time_slices[comm.rank + 1])
+
+            for j in range(comm.size):
                 y_trajectory[j] = comm.bcast(my_f_trajectory, root=j)
 
             my_g_value = self.g.trace(
-                diff_eq, y[rank], time_slices[rank], time_slices[rank + 1])[-1]
+                diff_eq,
+                y[comm.rank],
+                time_slices[comm.rank],
+                time_slices[comm.rank + 1])[-1]
+
             g_values = comm.allgather(my_g_value)
 
             for j, t in enumerate(time_slices[:-1]):
@@ -110,14 +74,4 @@ class Parareal:
                 y[j + 1] = updated_g_value + y_trajectory[j][-1] - g_values[j]
                 y_trajectory[j] += updated_g_value - g_values[j]
 
-        y_trajectory = y_trajectory.flatten()
-
-        comm.barrier()
-        end_time = MPI.Wtime()
-
-        if verbose and rank == 0:
-            self._print_results(
-                diff_eq, time_slices, y_coarse, y, y_trajectory)
-            print(f'Execution took {end_time - start_time}s')
-
-        return y_trajectory
+        return y_trajectory.flatten()
