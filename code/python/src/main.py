9001fad1bdd6ac4dbf634f5a62cbc4e2efd791c6
Viktor Csomor <viktor.csomor@gmail.com>
2020-05-09 00:52:44 +0100

    Plot solution

--- code/python/src/main.py
+++ code/python/src/main.py
@@ -1,3 +1,7 @@
+import re
+
+import matplotlib.pyplot as plt
+import numpy as np
 from mpi4py import MPI
 from sklearn.neural_network.multilayer_perceptron import MLPRegressor
 
@@ -5,6 +9,20 @@ from src.diff_eq import LotkaVolterraDiffEq
 from src.integrator import ExplicitMidpointMethod, RK4
 from src.operator import ConventionalOperator, MLOperator
 from src.parareal import Parareal
+
+
+def plot_y(y, solver_name):
+    t = np.linspace(diff_eq.t_0(), diff_eq.t_max(), len(y))
+    if len(y.shape) == 1:
+        plt.plot(t, y)
+    elif len(y.shape) == 2:
+        for i in range(y.shape[1]):
+            plt.plot(t, y[:, i])
+    plt.xlabel('t')
+    plt.ylabel('y')
+    file_name = re.sub('[^0-9a-zA-Z]+', '_', solver_name.lower())
+    plt.savefig(f'{file_name}.pdf')
+    plt.clf()
 
 
 def time_parallel_solver_and_print_result(
@@ -20,12 +38,13 @@ def time_parallel_solver_and_print_result(
     start_time = MPI.Wtime()
     if train:
         coarse_op.train_model(diff_eq, trainer, data_epochs, y_noise_var_coeff)
-    y_max = parallel_solver.solve(diff_eq, threshold)[-1]
+    y = parallel_solver.solve(diff_eq, threshold)
     comm.barrier()
     end_time = MPI.Wtime()
     if comm.rank == 0:
-        print(f'{solver_name} solution: {y_max}; '
+        print(f'{solver_name} solution: {y[-1]}; '
               f'execution took {end_time - start_time}s')
+        plot_y(y, solver_name)
 
 
 def time_operator_and_print_result(operator, operator_name):
