e31d26081852466cb331536691108b6f3794f1ad
Viktor Csomor <viktor.csomor@gmail.com>
2020-07-20 02:29:52 +0100

    Create a base class for ML operators and add a regression operator

--- code/python/src/main.py
+++ code/python/src/main.py
@@ -1,6 +1,7 @@
 from deepxde.maps import FNN
 from fipy import LinearLUSolver
 from mpi4py import MPI
+from sklearn.neural_network import MLPRegressor
 
 from src.core.boundary_condition import NeumannCondition, DirichletCondition
 from src.core.boundary_value_problem import BoundaryValueProblem
@@ -10,20 +11,23 @@ from src.core.initial_condition import ContinuousInitialCondition
 from src.core.initial_value_problem import InitialValueProblem
 from src.core.integrator import RK4
 from src.core.mesh import UniformGrid
-from src.core.operator import FVMOperator, PINNOperator, FDMOperator
+from src.core.operator import FVMOperator, PINNOperator, FDMOperator, \
+    RegressionOperator
 from src.core.parareal import Parareal
 from src.utils.plot import plot_ivp_solution
 from src.utils.time import time
 
-f = FVMOperator(LinearLUSolver(), .05)
+f = FVMOperator(LinearLUSolver(), .01)
 g = FDMOperator(
     RK4(),
     ThreePointCentralFiniteDifferenceMethod(),
-    .0025)
-g_ml = PINNOperator(.01)
+    .000025)
+g_reg = RegressionOperator(.01)
+g_pinn = PINNOperator(.01)
 
 parareal = Parareal(f, g)
-parareal_ml = Parareal(f, g_ml)
+parareal_reg = Parareal(f, g_reg)
+parareal_pinn = Parareal(f, g_pinn)
 
 threshold = .1
 
@@ -31,7 +35,7 @@ threshold = .1
 @time
 def create_ivp():
     diff_eq = DiffusionEquation(2)
-    mesh = UniformGrid(((0., 10.), (0., 10.)), (.1, .1))
+    mesh = UniformGrid(((0., 1.), (0., 1.)), (.01, .01))
     bvp = BoundaryValueProblem(
         diff_eq,
         mesh,
@@ -44,7 +48,7 @@ def create_ivp():
         lambda _: (0.,))
     return InitialValueProblem(
         bvp,
-        (0., 5.),
+        (0., .1),
         ic)
 
 
@@ -52,19 +56,27 @@ ivp = create_ivp()
 
 
 @time
-def train_coarse_ml():
+def train_coarse_reg():
+    g_reg.train(
+        ivp,
+        f,
+        MLPRegressor(hidden_layer_sizes=(50,) * 4))
+
+
+@time
+def train_coarse_pinn():
     diff_eq = ivp.boundary_value_problem.differential_equation
     x_dim = diff_eq.x_dimension + 1
     y_dim = diff_eq.y_dimension
-    g_ml.train(
+    g_pinn.train(
         ivp,
         FNN([x_dim] + [50] * 4 + [y_dim], "tanh", "Glorot normal"),
         {
-            'n_domain': 3000,
-            'n_initial': 400,
-            'n_boundary': 200,
+            'n_domain': 1000,
+            'n_initial': 100,
+            'n_boundary': 100,
             'n_test': 200,
-            'n_epochs': 5000,
+            'n_epochs': 3000,
             'optimiser': 'adam',
             'learning_rate': .001,
             'scipy_optimiser': 'L-BFGS-B'
@@ -82,8 +94,13 @@ def solve_serial_coarse():
 
 
 @time
-def solve_serial_coarse_ml():
-    return g_ml.trace(ivp)
+def solve_serial_coarse_reg():
+    return g_reg.trace(ivp)
+
+
+@time
+def solve_serial_coarse_pinn():
+    return g_pinn.trace(ivp)
 
 
 @time
@@ -92,8 +109,8 @@ def solve_parallel():
 
 
 @time
-def solve_parallel_ml():
-    return parareal_ml.solve(ivp, threshold)
+def solve_parallel_pinn():
+    return parareal_pinn.solve(ivp, threshold)
 
 
 def plot_solution(solve_func):
@@ -102,10 +119,14 @@ def plot_solution(solve_func):
         plot_ivp_solution(ivp, y, solve_func.__name__)
 
 
-# train_coarse_ml()
-# plot_solution(solve_serial_coarse_ml)
-# plot_solution(solve_parallel_ml)
+train_coarse_reg()
+plot_solution(solve_serial_coarse_reg)
+# plot_solution(solve_parallel_reg)
 
-plot_solution(solve_serial_fine)
-plot_solution(solve_serial_coarse)
-plot_solution(solve_parallel)
+# train_coarse_pinn()
+# plot_solution(solve_serial_coarse_pinn)
+# plot_solution(solve_parallel_pinn)
+
+# plot_solution(solve_serial_fine)
+# plot_solution(solve_serial_coarse)
+# plot_solution(solve_parallel)
