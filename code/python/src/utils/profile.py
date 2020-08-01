3b4368d60d67049935f22caa380c1654d0c5cfb0
Viktor Csomor <viktor.csomor@gmail.com>
2020-08-01 13:29:42 +0100

    Profile on one process at most

--- code/python/src/utils/profile.py
+++ code/python/src/utils/profile.py
@@ -2,17 +2,22 @@ import cProfile
 import functools
 from pstats import SortKey
 
+from mpi4py import MPI
+
 
 def profile(function):
     @functools.wraps(function)
     def wrapper(*args, **kwargs):
-        def no_arg_function():
-            function(*args, **kwargs)
+        if MPI.COMM_WORLD.rank == 0:
+            def no_arg_function():
+                function(*args, **kwargs)
 
-        cProfile.runctx(
-            'no_arg_function()',
-            globals(),
-            locals(),
-            sort=SortKey.TIME)
+            cProfile.runctx(
+                'no_arg_function()',
+                globals(),
+                locals(),
+                sort=SortKey.TIME)
+        else:
+            function(*args, **kwargs)
 
     return wrapper
