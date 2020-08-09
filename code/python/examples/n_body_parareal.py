05a65a4cfef1583f3ce9584faac8d5da862a4c48
Viktor Csomor <viktor.csomor@gmail.com>
2020-08-09 14:41:49 +0100

    Add parallel examples

---
+++ code/python/examples/n_body_parareal.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+from src.core.boundary_value_problem import BoundaryValueProblem
+from src.core.differential_equation import NBodyGravitationalEquation
+from src.core.initial_condition import ContinuousInitialCondition
+from src.core.initial_value_problem import InitialValueProblem
+from src.core.operator import ODEOperator
+from src.core.parareal import PararealOperator
+from src.utils.time import time_with_args
+
+n_planets = 10
+masses = np.random.randint(5e10, 5e12, n_planets)
+initial_positions = 40 * np.random.rand(n_planets * 3) - 20.
+initial_velocities = 5 * np.random.rand(n_planets * 3)
+
+diff_eq = NBodyGravitationalEquation(3, masses)
+bvp = BoundaryValueProblem(diff_eq)
+ic = ContinuousInitialCondition(
+    bvp,
+    lambda _: np.append(initial_positions, [initial_velocities]))
+ivp = InitialValueProblem(bvp, (0., 5.), ic)
+
+f = ODEOperator('RK45', 1e-6)
+g = ODEOperator('RK45', 1e-2)
+p = PararealOperator(f, g, .5)
+
+f_solution_name = 'n_body_fine'
+g_solution_name = 'n_body_coarse'
+p_solution_name = 'n_body_parareal'
+
+time_with_args(function_name=f_solution_name)(f.solve)(ivp) \
+    .plot(f_solution_name, n_images=20)
+time_with_args(function_name=g_solution_name)(g.solve)(ivp) \
+    .plot(g_solution_name, n_images=20)
+time_with_args(function_name=p_solution_name)(p.solve)(ivp) \
+    .plot(p_solution_name, n_images=20)
---
+++ code/python/examples/navier_stokes_2d_parareal.py
@@ -0,0 +1,39 @@
+from src.core.boundary_condition import DirichletCondition
+from src.core.boundary_value_problem import BoundaryValueProblem
+from src.core.differential_equation import NavierStokesEquation
+from src.core.differentiator import ThreePointCentralFiniteDifferenceMethod
+from src.core.initial_condition import ContinuousInitialCondition
+from src.core.initial_value_problem import InitialValueProblem
+from src.core.integrator import RK4, ExplicitMidpointMethod
+from src.core.mesh import UniformGrid
+from src.core.operator import FDMOperator
+from src.core.parareal import PararealOperator
+from src.utils.time import time_with_args
+
+diff_eq = NavierStokesEquation(2, 5000.)
+mesh = UniformGrid(((0., 10.), (0., 10.)), (.2, .2))
+bcs = (
+    (DirichletCondition(lambda x: (1., .1)),
+     DirichletCondition(lambda x: (.0, .0))),
+    (DirichletCondition(lambda x: (.0, .0)),
+     DirichletCondition(lambda x: (.0, .0)))
+)
+bvp = BoundaryValueProblem(diff_eq, mesh, bcs)
+ic = ContinuousInitialCondition(bvp, lambda x: (.0, .0))
+ivp = InitialValueProblem(bvp, (0., 20.), ic)
+
+f = FDMOperator(RK4(), ThreePointCentralFiniteDifferenceMethod(), .01)
+g = FDMOperator(
+    ExplicitMidpointMethod(), ThreePointCentralFiniteDifferenceMethod(), .1)
+p = PararealOperator(f, g, 1.)
+
+f_solution_name = 'navier_stokes_fine'
+g_solution_name = 'navier_stokes_coarse'
+p_solution_name = 'navier_stokes_parareal'
+
+time_with_args(function_name=f_solution_name)(f.solve)(ivp) \
+    .plot(f_solution_name, n_images=10)
+time_with_args(function_name=g_solution_name)(g.solve)(ivp) \
+    .plot(g_solution_name, n_images=10)
+time_with_args(function_name=p_solution_name)(p.solve)(ivp) \
+    .plot(p_solution_name, n_images=10)
