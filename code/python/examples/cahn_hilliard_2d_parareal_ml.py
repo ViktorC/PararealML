3bb5f2f1cb18eb98f4b5b72146f4d6a147137b07
Viktor Csomor <viktor.csomor@gmail.com>
2020-08-10 21:47:06 +0100

    Update Cahn-Hilliard experiment

--- code/python/examples/cahn_hilliard_2d_parareal_ml.py
+++ code/python/examples/cahn_hilliard_2d_parareal_ml.py
@@ -1,5 +1,5 @@
 import numpy as np
-from fipy import LinearCGSSolver
+from fipy import LinearLUSolver
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.multioutput import MultiOutputRegressor
@@ -9,22 +9,21 @@ from tensorflow.python.keras.layers import Dense
 from src.core.boundary_condition import NeumannCondition
 from src.core.boundary_value_problem import BoundaryValueProblem
 from src.core.differential_equation import CahnHilliardEquation
-from src.core.differentiator import ThreePointCentralFiniteDifferenceMethod
 from src.core.initial_condition import DiscreteInitialCondition
 from src.core.initial_value_problem import InitialValueProblem
-from src.core.integrator import RK4
 from src.core.mesh import UniformGrid
-from src.core.operator import FVMOperator, FDMOperator, \
-    StatefulRegressionOperator
+from src.core.operator import FVMOperator, StatefulRegressionOperator
 from src.utils.experiment import calculate_coarse_ml_operator_step_size, \
     run_parareal_ml_experiment
 from src.utils.ml import create_keras_regressor, limit_visible_gpus
-from src.utils.rand import SEEDS
+from src.utils.rand import SEEDS, set_random_seed
 
 limit_visible_gpus()
 
-diff_eq = CahnHilliardEquation(2, 1., .01)
-mesh = UniformGrid(((0., 10.), (0., 10.)), (.1, .1))
+set_random_seed(SEEDS[0])
+
+diff_eq = CahnHilliardEquation(2, 5., .5)
+mesh = UniformGrid(((0., 10.), (0., 10.)), (.5, .5))
 bcs = (
     (NeumannCondition(lambda x: (0., 0.)),
      NeumannCondition(lambda x: (0., 0.))),
@@ -34,16 +33,16 @@ bcs = (
 bvp = BoundaryValueProblem(diff_eq, mesh, bcs)
 ic = DiscreteInitialCondition(
     bvp,
-    .05 * np.random.uniform(-1., 1., bvp.y_shape(False)),
-    False)
+    .125 * np.random.uniform(-1., 1., bvp.y_shape(True)),
+    True)
 ivp = InitialValueProblem(bvp, (0., 5.), ic)
 
-f = FVMOperator(LinearCGSSolver(), .01)
-g = FDMOperator(RK4(), ThreePointCentralFiniteDifferenceMethod(), .01)
+f = FVMOperator(LinearLUSolver(), .005)
+g = FVMOperator(LinearLUSolver(), .025)
 g_ml = StatefulRegressionOperator(
     calculate_coarse_ml_operator_step_size(ivp), f.vertex_oriented)
 
-threshold = .1
+threshold = .5
 
 models = [
     LinearRegression(),
@@ -97,7 +96,8 @@ run_parareal_ml_experiment(
     g_ml,
     models,
     threshold,
-    SEEDS[:10],
+    SEEDS[:5],
+    solutions_per_trial=4,
     iterations=20,
-    noise_sd=(0., 1.),
+    noise_sd=(0., .005),
     model_names=model_names)
