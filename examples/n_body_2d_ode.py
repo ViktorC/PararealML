7d997c1ba19cc8e21be2513e7e8cd3c86f6a01b6
ViktorC <viktor.csomor@gmail.com>
2021-08-06 19:01:15 +0100

    Add 2D n-body simulation example

---
+++ examples/n_body_2d_ode.py
@@ -0,0 +1,51 @@
+import math
+
+from pararealml import *
+from pararealml.core.operators.ode import *
+
+g = 6.6743e-11
+
+minute = 60
+hour = 60 * minute
+day = 24 * hour
+
+m_earth = 5.97e24
+m_moon = 7.34767e22
+d_earth_moon = 3.844e8
+orbital_v_moon = math.sqrt(g * m_earth / d_earth_moon)
+
+v_x = 5.
+v_y = 5.
+v_length = math.sqrt(v_x ** 2 + v_y ** 2)
+
+masses = [m_earth, m_moon, m_moon, m_moon, m_moon]
+positions = [
+    0., 0.,
+    d_earth_moon, 0.,
+    0., d_earth_moon,
+    -d_earth_moon, 0.,
+    0., -d_earth_moon
+]
+velocities = [
+    0., 0.,
+    -v_x / v_length * orbital_v_moon, v_y / v_length * orbital_v_moon,
+    -v_y / v_length * orbital_v_moon, -v_x / v_length * orbital_v_moon,
+    v_x / v_length * orbital_v_moon, -v_y / v_length * orbital_v_moon,
+    v_y / v_length * orbital_v_moon, v_x / v_length * orbital_v_moon
+]
+
+diff_eq = NBodyGravitationalEquation(2, masses)
+cp = ConstrainedProblem(diff_eq)
+ic = ContinuousInitialCondition(
+    cp,
+    lambda _: positions + velocities)
+ivp = InitialValueProblem(cp, (0., 120 * day), ic)
+
+solver = ODEOperator('DOP853', minute)
+solution = solver.solve(ivp)
+solution.plot(
+    'pretty',
+    n_images=100,
+    smallest_marker_size=100.,
+    trajectory_line_width=.1,
+    trajectory_line_style='solid')
