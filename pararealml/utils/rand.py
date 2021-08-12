fdad87014326b74ad35ef5f45b0ad0604f313b75
ViktorC <viktor.csomor@gmail.com>
2021-08-12 02:00:36 +0100

    Update setting the seed to use TF2

--- pararealml/utils/rand.py
+++ pararealml/utils/rand.py
@@ -33,4 +33,4 @@ def set_random_seed(seed: int):
     :param seed: the random seed value to use
     """
     np.random.seed(seed)
-    tf.compat.v1.set_random_seed(seed)
+    tf.random.set_seed(seed)
