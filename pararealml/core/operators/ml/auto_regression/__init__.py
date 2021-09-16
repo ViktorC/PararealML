df6b234dda025f45166c03b2ff0ef24fbb6e6a47
ViktorC <viktor.csomor@gmail.com>
2021-09-16 18:34:35 +0100

    Include the RegressionModel type definition in the wildcard import

--- pararealml/core/operators/ml/auto_regression/__init__.py
+++ pararealml/core/operators/ml/auto_regression/__init__.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import
 
 from pararealml.core.operators.ml.auto_regression.auto_regression_operator \
-    import AutoRegressionOperator
+    import AutoRegressionOperator, RegressionModel
 from pararealml.core.operators.ml.auto_regression.sklearn_keras_regressor \
     import SKLearnKerasRegressor
 
 __all__ = [
     'AutoRegressionOperator',
+    'RegressionModel',
     'SKLearnKerasRegressor'
 ]
