0fc1f1d9c77fb0a8fab30e59e9916f08494cfd45
ViktorC <viktor.csomor@gmail.com>
2021-09-20 00:07:55 +0100

    Update 1D diffusion Parareal ML example yet again

--- examples/diffusion_1d_parareal_ml.py
+++ examples/diffusion_1d_parareal_ml.py
@@ -16,7 +16,7 @@ from pararealml.utils.time import time_with_args
 limit_tf_visible_gpus()
 set_random_seed(SEEDS[0])
 
-diff_eq = DiffusionEquation(1, .025)
+diff_eq = DiffusionEquation(1, .125)
 mesh = Mesh([(0., 1.)], (.02,))
 bcs = [
     (NeumannBoundaryCondition(
@@ -25,7 +25,7 @@ bcs = [
          lambda x, t: np.zeros((len(x), 1)), is_static=True)),
 ]
 cp = ConstrainedProblem(diff_eq, mesh, bcs)
-t_interval = (0., 5.)
+t_interval = (0., 1.)
 ic_mean = .5
 ic = GaussianInitialCondition(
     cp,
@@ -36,11 +36,11 @@ ivp = InitialValueProblem(cp, t_interval, ic)
 f = FDMOperator(
     CrankNicolsonMethod(),
     ThreePointCentralFiniteDifferenceMethod(),
-    .0001)
+    .00001)
 g = FDMOperator(
     ForwardEulerMethod(),
     ThreePointCentralFiniteDifferenceMethod(),
-    .0005)
+    .000025)
 
 
 def auc(y: np.ndarray, d_x: float) -> float:
@@ -52,7 +52,7 @@ def auc(y: np.ndarray, d_x: float) -> float:
 
 ic_auc = auc(ic.discrete_y_0(True), mesh.d_x[0])
 
-ar_don = AutoRegressionOperator(1.25, g.vertex_oriented)
+ar_don = AutoRegressionOperator(.25, g.vertex_oriented)
 train_score, test_score = time_with_args(function_name='ar_don_train')(
     ar_don.train
 )(
@@ -70,38 +70,37 @@ train_score, test_score = time_with_args(function_name='ar_don_train')(
         ),
         optimizer=optimizers.Adam(
             learning_rate=optimizers.schedules.ExponentialDecay(
-                5e-3, decay_steps=200, decay_rate=.97
+                5e-3, decay_steps=200, decay_rate=.98
             )
         ),
-        batch_size=11008,
+        batch_size=16000,
         epochs=10000,
         verbose=True
     ),
-    400,
-    lambda t, y: (y - ic_auc) * np.random.normal(1., t / 100.) + ic_auc
+    600,
+    lambda t, y: (y - ic_auc) * np.random.normal(1., t / 10.) + ic_auc
 )
-if MPI.COMM_WORLD.rank == 0:
-    print('AR train score:', train_score)
-    print('AR test score:', test_score)
+print('AR train score:', train_score)
+print('AR test score:', test_score)
 
 training_y_0_functions = [
     lambda x, _scale=scale: (ic.y_0(x) - ic_auc) * _scale + ic_auc
-    for scale in np.linspace(0., 1., 100, endpoint=True)
+    for scale in np.linspace(.05, 1., 80, endpoint=True)
 ]
 test_y_0_functions = [
     lambda x, _scale=scale: (ic.y_0(x) - ic_auc) * _scale + ic_auc
     for scale in np.random.random(10)
 ]
 sampler = UniformRandomCollocationPointSampler()
-pidon = PIDONOperator(sampler, 1.25, g.vertex_oriented, offset_t_0=True)
+pidon = PIDONOperator(sampler, .25, g.vertex_oriented, offset_t_0=True)
 time_with_args(function_name='pidon_train')(pidon.train)(
     cp,
-    t_interval,
+    (0., .25),
     training_data_args=DataArgs(
         y_0_functions=training_y_0_functions,
         n_domain_points=2000,
         n_boundary_points=200,
-        n_batches=20,
+        n_batches=10,
     ),
     test_data_args=DataArgs(
         y_0_functions=test_y_0_functions,
@@ -115,19 +114,17 @@ time_with_args(function_name='pidon_train')(pidon.train)(
         trunk_hidden_layer_sizes=[100] * 6,
     ),
     optimization_args=OptimizationArgs(
-        optimizer={
-            'class_name': 'Adam',
-            'config': {
-                'learning_rate': optimizers.schedules.ExponentialDecay(
-                    5e-3, decay_steps=400, decay_rate=.98)
-            }
-        },
+        optimizer=optimizers.Adam(
+            learning_rate=optimizers.schedules.ExponentialDecay(
+                5e-3, decay_steps=200, decay_rate=.98
+            )
+        ),
         epochs=10000,
-        ic_loss_weight=15.,
+        ic_loss_weight=20.,
     )
 )
 
-tol = 1e-7
+tol = 1e-4
 p = PararealOperator(f, g, tol)
 p_ar_don = PararealOperator(f, ar_don, tol)
 p_pidon = PararealOperator(f, pidon, tol)
@@ -152,13 +149,13 @@ p_ar_don_sol = time_with_args(function_name=p_ar_don_solution_name)(
 p_pidon_sol = time_with_args(function_name=p_pidon_solution_name)(
     p_pidon.solve)(ivp)
 
-f_sol.plot(f_solution_name, True)
-g_sol.plot(g_solution_name, True)
-g_ar_don_sol.plot(g_ar_don_solution_name, True)
-g_pidon_sol.plot(g_pidon_solution_name, True)
-p_sol.plot(p_solution_name, True)
-p_ar_don_sol.plot(p_ar_don_solution_name, True)
-p_pidon_sol.plot(p_pidon_solution_name, True)
+f_sol.plot(f'{MPI.COMM_WORLD.rank}_{f_solution_name}')
+g_sol.plot(f'{MPI.COMM_WORLD.rank}_{g_solution_name}')
+g_ar_don_sol.plot(f'{MPI.COMM_WORLD.rank}_{g_ar_don_solution_name}')
+g_pidon_sol.plot(f'{MPI.COMM_WORLD.rank}_{g_pidon_solution_name}')
+p_sol.plot(f'{MPI.COMM_WORLD.rank}_{p_solution_name}')
+p_ar_don_sol.plot(f'{MPI.COMM_WORLD.rank}_{p_ar_don_solution_name}')
+p_pidon_sol.plot(f'{MPI.COMM_WORLD.rank}_{p_pidon_solution_name}')
 
 diff = f_sol.diff([
     g_sol,
@@ -170,26 +167,25 @@ diff = f_sol.diff([
 ])
 rms_diffs = np.sqrt(np.square(np.stack(diff.differences)).mean(axis=(2, 3)))
 
-if MPI.COMM_WORLD.rank == 0:
-    plot_rms_solution_diffs(
-        diff.matching_time_points,
-        rms_diffs[:3, ...],
-        np.zeros_like(rms_diffs[:3, ...]),
-        [
-            'fdm_coarse',
-            'ar_don_coarse',
-            'pidon_coarse',
-        ],
-        'coarse_operator_accuracy'
-    )
-    plot_rms_solution_diffs(
-        diff.matching_time_points,
-        rms_diffs[3:, ...],
-        np.zeros_like(rms_diffs[3:, ...]),
-        [
-            'parareal_fdm',
-            'parareal_ar_don',
-            'parareal_pidon'
-        ],
-        'parareal_operator_accuracy'
-    )
+plot_rms_solution_diffs(
+    diff.matching_time_points,
+    rms_diffs[:1, ...],
+    np.zeros_like(rms_diffs[:1, ...]),
+    [
+        'fdm_coarse',
+        'ar_don_coarse',
+        'pidon_coarse',
+    ],
+    f'{MPI.COMM_WORLD.rank}_coarse_operator_accuracy'
+)
+plot_rms_solution_diffs(
+    diff.matching_time_points,
+    rms_diffs[1:, ...],
+    np.zeros_like(rms_diffs[1:, ...]),
+    [
+        'parareal_fdm',
+        'parareal_ar_don',
+        'parareal_pidon'
+    ],
+    f'{MPI.COMM_WORLD.rank}_parareal_operator_accuracy'
+)
