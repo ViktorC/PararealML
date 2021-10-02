98871ebb2e3c378b7b18a0b9e21e14ca9f236a0e
ViktorC <viktor.csomor@gmail.com>
2021-10-02 19:11:37 +0100

    Update Lotka-Volterra Parareal ML example

--- examples/lotka_volterra_parareal_ml.py
+++ examples/lotka_volterra_parareal_ml.py
@@ -24,22 +24,24 @@ ivp = InitialValueProblem(cp, (0., 10.), ic)
 f = FDMOperator(
     ForwardEulerMethod(),
     ThreePointCentralDifferenceMethod(),
-    1.25e-5
+    2.5e-5
 )
 g = FDMOperator(
     ForwardEulerMethod(),
     ThreePointCentralDifferenceMethod(),
-    1e-4
+    2.5e-4
 )
 
 g_sol = g.solve(ivp)
-y_0_functions = [ic.y_0] * 50 + [
+y_0_functions = [ic.y_0] * 25 + [
     lambda x, _y_0=y_0: _y_0
     for y_0 in g_sol.discrete_y(g.vertex_oriented)[
-        np.random.choice(80000, 4350, False)
+        np.random.choice(32000, 10975, False)
     ]
 ]
 np.random.shuffle(y_0_functions)
+training_y_0_functions = y_0_functions[:10000]
+test_y_0_functions = y_0_functions[10000:]
 sampler = UniformRandomCollocationPointSampler()
 pidon = PIDONOperator(
     sampler,
@@ -51,16 +53,16 @@ time_with_args(function_name='pidon_train')(pidon.train)(
     cp,
     (0., 2.5),
     training_data_args=DataArgs(
-        y_0_functions=y_0_functions[:4000],
-        n_domain_points=4000,
-        n_batches=800,
-        n_ic_repeats=400
+        y_0_functions=training_y_0_functions,
+        n_domain_points=5000,
+        n_batches=5000,
+        n_ic_repeats=1000
     ),
     test_data_args=DataArgs(
-        y_0_functions=y_0_functions[4000:],
-        n_domain_points=200,
-        n_batches=4,
-        n_ic_repeats=20
+        y_0_functions=test_y_0_functions,
+        n_domain_points=500,
+        n_batches=50,
+        n_ic_repeats=50
     ),
     model_args=ModelArgs(
         latent_output_size=50,
@@ -70,10 +72,10 @@ time_with_args(function_name='pidon_train')(pidon.train)(
     optimization_args=OptimizationArgs(
         optimizer=optimizers.Adam(
             learning_rate=optimizers.schedules.ExponentialDecay(
-                5e-3, decay_steps=200, decay_rate=.98
+                2e-3, decay_steps=500, decay_rate=.98
             )
         ),
-        epochs=125,
+        epochs=25,
         diff_eq_loss_weight=10.
     )
 )
@@ -98,15 +100,15 @@ train_score, test_score = time_with_args(function_name='ar_don_train')(
         ),
         optimizer=optimizers.Adam(
             learning_rate=optimizers.schedules.ExponentialDecay(
-                5e-3, decay_steps=20, decay_rate=.98
+                1e-3, decay_steps=40, decay_rate=.98
             )
         ),
-        batch_size=16000,
+        batch_size=6400,
         epochs=4000,
         verbose=True
     ),
-    5000,
-    lambda t, y: y + np.random.normal(0., t / 30000., size=y.shape)
+    2000,
+    lambda t, y: y + np.random.normal(0., t / 3750., size=y.shape)
 )
 print('AR train score:', train_score)
 print('AR test score:', test_score)
@@ -120,16 +122,22 @@ g_pidon_solution_name = f'{prefix}_coarse_pidon'
 f_sol = time_with_args(function_name=f_solution_name)(f.solve)(ivp)
 g_sol = time_with_args(function_name=g_solution_name)(g.solve)(ivp)
 g_ar_don_sol = time_with_args(function_name=g_ar_don_solution_name)(
-    ar_don.solve)(ivp)
+    ar_don.solve
+)(ivp)
 g_pidon_sol = time_with_args(function_name=g_pidon_solution_name)(
-    pidon.solve)(ivp)
+    pidon.solve
+)(ivp)
 
 f_sol.plot(f_solution_name)
 g_sol.plot(g_solution_name)
 g_ar_don_sol.plot(g_ar_don_solution_name)
 g_pidon_sol.plot(g_pidon_solution_name)
 
-diff = f_sol.diff([g_sol, g_ar_don_sol, g_pidon_sol])
+diff = f_sol.diff([
+    g_sol,
+    g_ar_don_sol,
+    g_pidon_sol
+])
 rms_diffs = np.sqrt(np.square(np.stack(diff.differences)).sum(axis=2))
 print(f'{prefix} - RMS differences:', repr(rms_diffs))
 print(
@@ -148,13 +156,13 @@ plot_rms_solution_diffs(
     [
         'fdm',
         'ar_don',
-        'pidon',
+        'pidon'
     ],
     f'{prefix}_coarse_operator_accuracy'
 )
 
 for p_kwargs in [
-    {'tol': 1e-3, 'max_iterations': 99},
+    {'tol': 3e-3, 'max_iterations': 99},
     {'tol': 0., 'max_iterations': 1},
     {'tol': 0., 'max_iterations': 2},
     {'tol': 0., 'max_iterations': 3},
@@ -164,41 +172,52 @@ for p_kwargs in [
     p_ar_don = PararealOperator(f, ar_don, **p_kwargs)
     p_pidon = PararealOperator(f, pidon, **p_kwargs)
 
-    p_prefix = f'{prefix}_parareal_max_iterations_{p_kwargs["max_iterations"]}'
+    p_prefix = \
+        f'lotka_volterra_parareal_max_iterations_{p_kwargs["max_iterations"]}'
     p_solution_name = f'{p_prefix}_fdm'
     p_ar_don_solution_name = f'{p_prefix}_ar_don'
     p_pidon_solution_name = f'{p_prefix}_pidon'
 
-    p_sol = time_with_args(function_name=p_solution_name)(p.solve)(ivp)
-    p_ar_don_sol = time_with_args(function_name=p_ar_don_solution_name)(
-        p_ar_don.solve)(ivp)
-    p_pidon_sol = time_with_args(function_name=p_pidon_solution_name)(
-        p_pidon.solve)(ivp)
+    p_sol = time_with_args(
+      function_name=p_solution_name,
+      print_on_first_rank_only=True
+    )(p.solve)(ivp)
+    p_ar_don_sol = time_with_args(
+      function_name=p_ar_don_solution_name,
+      print_on_first_rank_only=True
+    )(p_ar_don.solve)(ivp)
+    p_pidon_sol = time_with_args(
+      function_name=p_pidon_solution_name,
+      print_on_first_rank_only=True
+    )(p_pidon.solve)(ivp)
 
-    p_sol.plot(p_solution_name)
-    p_ar_don_sol.plot(p_ar_don_solution_name)
-    p_pidon_sol.plot(p_pidon_solution_name)
+    if MPI.COMM_WORLD.rank == 0:
+        p_sol.plot(p_solution_name)
+        p_ar_don_sol.plot(p_ar_don_solution_name)
+        p_pidon_sol.plot(p_pidon_solution_name)
 
-    p_diff = f_sol.diff([p_sol, p_ar_don_sol, p_pidon_sol])
-    p_rms_diffs = np.sqrt(np.square(np.stack(p_diff.differences)).sum(axis=2))
-    print(f'{p_prefix} - RMS differences:', repr(p_rms_diffs))
-    print(
-        f'{p_prefix} - max RMS differences:',
-        p_rms_diffs.max(axis=-1, keepdims=True)
-    )
-    print(
-        f'{p_prefix} - mean RMS differences:',
-        p_rms_diffs.mean(axis=-1, keepdims=True)
-    )
+        p_diff = f_sol.diff([p_sol, p_ar_don_sol, p_pidon_sol])
+        p_rms_diffs = np.sqrt(
+            np.square(np.stack(p_diff.differences)).sum(axis=2)
+        )
+        print(f'{p_prefix} - RMS differences:', repr(p_rms_diffs))
+        print(
+            f'{p_prefix} - max RMS differences:',
+            p_rms_diffs.max(axis=-1, keepdims=True)
+        )
+        print(
+            f'{p_prefix} - mean RMS differences:',
+            p_rms_diffs.mean(axis=-1, keepdims=True)
+        )
 
-    plot_rms_solution_diffs(
-        p_diff.matching_time_points,
-        p_rms_diffs,
-        np.zeros_like(p_rms_diffs),
-        [
-            'fdm',
-            'ar_don',
-            'pidon'
-        ],
-        f'{p_prefix}_operator_accuracy'
-    )
+        plot_rms_solution_diffs(
+            p_diff.matching_time_points,
+            p_rms_diffs,
+            np.zeros_like(p_rms_diffs),
+            [
+                'fdm',
+                'ar_don',
+                'pidon'
+            ],
+            f'{p_prefix}_operator_accuracy'
+        )
