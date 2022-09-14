57cdd984b4513b64e070d51111d99fb3d8d00bc8
ViktorC <viktor.csomor@gmail.com>
2022-09-15 00:06:27 +0100

    Add support for specifying Parareal termination condition separately for different y dimensions

--- pararealml/operators/parareal/parareal_operator.py
+++ pararealml/operators/parareal/parareal_operator.py
@@ -1,5 +1,5 @@
 import sys
-from typing import Callable, Optional
+from typing import Callable, Sequence, Union
 
 import numpy as np
 from mpi4py import MPI
@@ -20,38 +20,82 @@ class PararealOperator(Operator):
         self,
         f: Operator,
         g: Operator,
-        tol: float,
-        max_iterations: int = sys.maxsize,
-        termination_condition_func: Optional[
-            Callable[[np.ndarray], bool]
+        termination_condition: Union[
+            float, Sequence[float], Callable[[np.ndarray, np.ndarray], bool]
         ] = None,
+        max_iterations: int = sys.maxsize,
     ):
         """
         :param f: the fine operator
         :param g: the coarse operator
-        :param tol: the minimum absolute value of the largest update to
-            the solution required to perform another corrective iteration; if
-            all updates are smaller than this threshold, the solution is
-            considered accurate enough
+        :param termination_condition: the termination condition provided in one
+            of the following forms: a floating point number representing the
+            minimum Euclidean norm of the largest update to the solution
+            required to perform another corrective iteration (if all updates
+            are smaller than this threshold, the solution is considered
+            accurate enough); a sequence of such numbers with one for each
+            dimension of y in case y is vector-valued; or a predicate function
+            that takes both the previous and the new end-point estimates of the
+            solutions of the sub-IVPs and returns a Boolean denoting whether
+            the termination condition is met
         :param max_iterations: the maximum number of iterations to perform
             (effective only if it is less than the number of executing
             processes and the accuracy requirements are not satisfied in fewer
             iterations)
-        :param termination_condition_func: a predicate function that takes the
-            latest sub-solution end point estimates and returns a Boolean
-            denoting whether the termination condition is met
         """
         super(PararealOperator, self).__init__(f.d_t, f.vertex_oriented)
 
         self._f = f
         self._g = g
-        self._tol = tol
+        self._termination_condition = termination_condition
         self._max_iterations = max_iterations
-        self._termination_condition_func = (
-            termination_condition_func
-            if termination_condition_func is not None
-            else lambda _: False
-        )
+
+    def _should_terminate(
+        self, old_y_end_points: np.ndarray, new_y_end_points: np.ndarray
+    ) -> bool:
+        """
+        Determines whether the termination condition is met based on the old
+        and new values of the end-point estimates of the solutions of the
+        sub-IVPs.
+
+        :param old_y_end_points: the old end point estimates
+        :param new_y_end_points: the new end point estimates
+        :return: whether the termination condition is met
+        """
+        if callable(self._termination_condition):
+            return self._termination_condition(
+                old_y_end_points, new_y_end_points
+            )
+
+        y_dim = old_y_end_points.shape[-1]
+
+        if isinstance(self._termination_condition, Sequence):
+            if len(self._termination_condition) != y_dim:
+                raise ValueError(
+                    "length of update tolerances "
+                    f"({len(self._termination_condition)}) must match number "
+                    f"of y dimensions ({y_dim})"
+                )
+
+            update_tolerances = np.array(self._termination_condition)
+
+        else:
+            update_tolerances = np.array([self._termination_condition] * y_dim)
+
+        max_diff_norms = np.empty(y_dim)
+        for y_ind in range(y_dim):
+            max_diff_norm = 0.0
+            for new_y_end_point, old_y_end_point in zip(
+                new_y_end_points[..., y_ind], old_y_end_points[..., y_ind]
+            ):
+                max_diff_norm = np.maximum(
+                    max_diff_norm,
+                    np.linalg.norm(new_y_end_point - old_y_end_point),
+                )
+
+            max_diff_norms[y_ind] = max_diff_norm
+
+        return all(max_diff_norms < update_tolerances)
 
     def solve(
         self, ivp: InitialValueProblem, parallel_enabled: bool = True
@@ -118,8 +162,7 @@ class PararealOperator(Operator):
             correction = sub_y_fine[-1] - y_coarse_end_points[comm.rank]
             comm.Allgather([correction, MPI.DOUBLE], [corrections, MPI.DOUBLE])
 
-            max_update = 0.0
-
+            old_y_end_points = np.copy(y_border_points[1:])
             for j in range(i, comm.size):
                 if j > i:
                     sub_ivp = InitialValueProblem(
@@ -135,16 +178,11 @@ class PararealOperator(Operator):
                     sub_y_coarse = g.solve(sub_ivp).discrete_y(vertex_oriented)
                     y_coarse_end_points[j] = sub_y_coarse[-1]
 
-                new_y_end_point = y_coarse_end_points[j] + corrections[j]
-                max_update = np.maximum(
-                    max_update,
-                    np.linalg.norm(new_y_end_point - y_border_points[j + 1]),
+                y_border_points[j + 1] = (
+                    y_coarse_end_points[j] + corrections[j]
                 )
-                y_border_points[j + 1] = new_y_end_point
 
-            if max_update < self._tol or self._termination_condition_func(
-                y_border_points[1:]
-            ):
+            if self._should_terminate(old_y_end_points, y_border_points[1:]):
                 break
 
         t = discretize_time_domain(ivp.t_interval, f.d_t)[1:]
--- tests/operators/parareal/test_parareal_operator.py
+++ tests/operators/parareal/test_parareal_operator.py
@@ -6,6 +6,7 @@ from pararealml.constrained_problem import ConstrainedProblem
 from pararealml.differential_equation import (
     DiffusionEquation,
     LorenzEquation,
+    LotkaVolterraEquation,
     PopulationGrowthEquation,
 )
 from pararealml.initial_condition import (
@@ -45,6 +46,20 @@ def test_parareal_operator_with_wrong_g_time_step_size():
     f = FDMOperator(RK4(), ThreePointCentralDifferenceMethod(), 0.1)
     g = FDMOperator(RK4(), ThreePointCentralDifferenceMethod(), 0.6)
     p = PararealOperator(f, g, 0.001)
+
+    with pytest.raises(ValueError):
+        p.solve(ivp)
+
+
+def test_parareal_operator_with_wrong_termination_condition_length():
+    diff_eq = LotkaVolterraEquation()
+    cp = ConstrainedProblem(diff_eq)
+    ic = ContinuousInitialCondition(cp, lambda _: np.array([10, 5]))
+    ivp = InitialValueProblem(cp, (0.0, 10.0), ic)
+
+    f = FDMOperator(RK4(), ThreePointCentralDifferenceMethod(), 0.01)
+    g = FDMOperator(RK4(), ThreePointCentralDifferenceMethod(), 0.1)
+    p = PararealOperator(f, g, [0.01, 0.02, 0.03])
 
     with pytest.raises(ValueError):
         p.solve(ivp)
@@ -109,7 +124,7 @@ def test_parareal_operator_on_pde():
 
     f = FDMOperator(RK4(), ThreePointCentralDifferenceMethod(), 0.05)
     g = FDMOperator(RK4(), ThreePointCentralDifferenceMethod(), 0.5)
-    p = PararealOperator(f, g, 0.005)
+    p = PararealOperator(f, g, [0.005])
 
     f_solution = f.solve(ivp)
     p_solution = p.solve(ivp)
@@ -118,3 +133,29 @@ def test_parareal_operator_on_pde():
     assert p_solution.d_t == f_solution.d_t
     assert np.array_equal(p_solution.t_coordinates, f_solution.t_coordinates)
     assert np.allclose(p_solution.discrete_y(), f_solution.discrete_y())
+
+
+def test_parareal_operator_with_termination_condition_function():
+    diff_eq = PopulationGrowthEquation()
+    cp = ConstrainedProblem(diff_eq)
+    ic = ContinuousInitialCondition(cp, lambda _: np.array([5.0]))
+    ivp = InitialValueProblem(cp, (0.0, 10.0), ic)
+
+    f = FDMOperator(RK4(), ThreePointCentralDifferenceMethod(), 0.1)
+    g = FDMOperator(RK4(), ThreePointCentralDifferenceMethod(), 0.5)
+    p = PararealOperator(
+        f,
+        g,
+        lambda old_y_end_points, new_y_end_points: np.abs(
+            new_y_end_points - old_y_end_points
+        ).sum()
+        < 0.005,
+    )
+
+    f_solution = f.solve(ivp)
+    p_solution = p.solve(ivp)
+
+    assert p_solution.vertex_oriented == f_solution.vertex_oriented
+    assert p_solution.d_t == f_solution.d_t
+    assert np.array_equal(p_solution.t_coordinates, f_solution.t_coordinates)
+    assert np.array_equal(p_solution.discrete_y(), f_solution.discrete_y())
