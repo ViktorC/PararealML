f802d10be6cfe61fce52dae421098bde2e0380d4
ViktorC <viktor.csomor@gmail.com>
2023-01-08 01:09:06 +0000

    Bump version number to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pararealml',
-    version='0.2.0',
+    version='0.3.0',
     description='A machine learning boosted parallel-in-time differential '
                 'equation solver framework',
     url='https://github.com/ViktorC/PararealML',
